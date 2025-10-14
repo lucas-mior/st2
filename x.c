8246df1ab624ccfc0b073a47a273e97f682fcea3
lucas-mior <lucas.mior@tutamail.com>
2025-10-14 15:32:22 -0300

    use long, descriptive names (zoomreset -> zoom_reset)

--- x.c
+++ x.c
@@ -57,7 +57,7 @@ static void toggle_numlock(const Arg *);
 static void sel_paste(const Arg *);
 static void zoom(const Arg *);
 static void zoom_abs(const Arg *);
-static void zoomreset(const Arg *);
+static void zoom_reset(const Arg *);
 static void ttysend(const Arg *);
 
 /* config.h for applying patches and the configuration. */
@@ -312,7 +312,7 @@ zoom_abs(const Arg *arg)
 }
 
 void
-zoomreset(const Arg *arg)
+zoom_reset(const Arg *arg)
 {
 	Arg larg;
 
