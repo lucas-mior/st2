723a8ceeabd1b0700ba40a1aee92a98138b36097
lucas-mior <lucas.mior@tutamail.com>
2025-10-15 15:12:58 -0300

    wip

--- st.h
+++ st.h
@@ -7,6 +7,22 @@
 #include <stddef.h>
 #include <sys/types.h>
 
+typedef unsigned char uchar;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef size_t usize;
+typedef ssize_t isize;
+
+typedef uint_least32_t Rune;
+
 /* macros */
 #define MIN(a, b)		((a) < (b) ? (a) : (b))
 #define MAX(a, b)		((a) < (b) ? (b) : (a))
@@ -63,22 +79,6 @@ enum selection_snap {
 	SELECTION_SNAP_WORD = 1,
 	SELECTION_SNAP_LINE = 2
 };
-
-typedef unsigned char uchar;
-
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-typedef size_t usize;
-typedef ssize_t isize;
-
-typedef uint_least32_t Rune;
 
 #define Glyph Glyph_
 typedef struct {
