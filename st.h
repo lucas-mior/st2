39f94a492192f47fa9d231e192fa9956c2e785ee
lucas-mior <lucas.mior@tutamail.com>
2026-05-02 10:09:06 -0300

    cleanup

--- st.h
+++ st.h
@@ -14,20 +14,7 @@
 #include <X11/Xft/Xft.h>
 #include <X11/XKBlib.h>
 
-typedef unsigned char uchar;
-typedef unsigned long ulong;
-
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-typedef size_t usize;
-typedef ssize_t isize;
+#include "cbase/util.c"
 
 #define UTF_INVALID 0xFFFD
 #define UTF_SIZ 4
