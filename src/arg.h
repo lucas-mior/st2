6f1ad9ab674ba858658ae5a25907914d6f0b7260
lucas-mior <lucas.mior@tutamail.com>
2026-05-02 17:42:45 -0300

    apply clang style to arg.h

--- src/arg.h
+++ src/arg.h
@@ -9,42 +9,43 @@
 static char *argv0;
 
 /* use main(int32 argc, char *argv[]) */
-#define ARGBEGIN	do { for (argv0 = *argv, argv++, argc--;\
-					argv[0] && argv[0][0] == '-'\
-					&& argv[0][1];\
-					argc--, argv++) {\
-				char argc_;\
-				char **argv_;\
-				int32 brk_;\
-				int32 i_;\
-				if (argv[0][1] == '-' && argv[0][2] == '\0') {\
-					argv++;\
-					argc--;\
-					break;\
-				}\
-				for (i_ = 1, brk_ = 0, argv_ = argv;\
-						argv[0][i_] && !brk_;\
-						i_++) {\
-					if (argv_ != argv)\
-						break;\
-					argc_ = argv[0][i_];\
-					switch (argc_)
+#define ARGBEGIN                                                               \
+  do {                                                                         \
+    for (argv0 = *argv, argv++, argc--;                                        \
+         argv[0] && argv[0][0] == '-' && argv[0][1]; argc--, argv++) {         \
+      char argc_;                                                              \
+      char **argv_;                                                            \
+      int32 brk_;                                                              \
+      int32 i_;                                                                \
+      if (argv[0][1] == '-' && argv[0][2] == '\0') {                           \
+        argv++;                                                                \
+        argc--;                                                                \
+        break;                                                                 \
+      }                                                                        \
+      for (i_ = 1, brk_ = 0, argv_ = argv; argv[0][i_] && !brk_; i_++) {       \
+        if (argv_ != argv)                                                     \
+          break;                                                               \
+        argc_ = argv[0][i_];                                                   \
+        switch (argc_)
 
-#define ARGEND			}\
-			} } while (0)
+#define ARGEND                                                                 \
+  }                                                                            \
+  }                                                                            \
+  }                                                                            \
+  while (0)
 
-#define ARGC()		argc_
+#define ARGC() argc_
 
-#define EARGF(x)	((argv[0][i_+1] == '\0' && argv[1] == NULL)?\
-				((x), abort(), (char *)0) :\
-				(brk_ = 1, (argv[0][i_+1] != '\0')?\
-					(&argv[0][i_+1]) :\
-					(argc--, argv++, argv[0])))
+#define EARGF(x)                                                               \
+  ((argv[0][i_ + 1] == '\0' && argv[1] == NULL)                                \
+       ? ((x), abort(), (char *)0)                                             \
+       : (brk_ = 1, (argv[0][i_ + 1] != '\0') ? (&argv[0][i_ + 1])             \
+                                              : (argc--, argv++, argv[0])))
 
-#define ARGF()		((argv[0][i_+1] == '\0' && argv[1] == NULL)?\
-				(char *)0 :\
-				(brk_ = 1, (argv[0][i_+1] != '\0')?\
-					(&argv[0][i_+1]) :\
-					(argc--, argv++, argv[0])))
+#define ARGF()                                                                 \
+  ((argv[0][i_ + 1] == '\0' && argv[1] == NULL)                                \
+       ? (char *)0                                                             \
+       : (brk_ = 1, (argv[0][i_ + 1] != '\0') ? (&argv[0][i_ + 1])             \
+                                              : (argc--, argv++, argv[0])))
 
 #endif
