c70c8cb868c92c7904175c51ca5d59e7b9a533cb
lucas-mior <lucas.mior@tutamail.com>
2026-05-02 15:56:21 -0300

    organize

--- src/st.h
+++ src/st.h
@@ -57,17 +57,19 @@
 #define IS_CONTROL_C1(c) (BETWEEN(c, 0x80, 0x9f))
 #define IS_CONTROl(c) (IS_CONTROL_C0(c) || IS_CONTROL_C1(c))
 #define IS_DELIM(u) (u && wcschr(CONF_WORD_DELIMITERS, (wchar_t)u))
-#define TERM_LINE(y)                                                                                \
-    ((y) < term.lines_scrolled_up                                                                   \
-         ? term.hist[(term.i_hist + (y) - term.lines_scrolled_up + 1 + HISTORY_SIZE)                \
-                     % HISTORY_SIZE]                                                                \
+#define TERM_LINE(y)                                                                                 \
+    ((y) < term.lines_scrolled_up                                                                    \
+         ? term.hist[(term.i_hist + (y) - term.lines_scrolled_up + 1 + HISTORY_SIZE) % HISTORY_SIZE] \
          : term.lines[(y) - term.lines_scrolled_up])
 
 #define TERM_LINE_ABS(y)                                                                            \
-    ((y) < 0 ? term.hist[(term.i_hist + (y) + 1 + HISTORY_SIZE) % HISTORY_SIZE] : term.lines[(y)])
+    ((y) < 0 \
+	     ? term.hist[(term.i_hist + (y) + 1 + HISTORY_SIZE) % HISTORY_SIZE] \
+		 : term.lines[(y)])
 #define TERM_LINE_HIST(y)                                                                           \
-    ((y) <= HISTORY_SIZE - term.nrows + 2 ? term.hist[(y)]                                          \
-                                          : term.lines[(y - HISTORY_SIZE + term.nrows - 3)])
+    ((y) <= HISTORY_SIZE - term.nrows + 2 \
+	     ? term.hist[(y)]                                          \
+         : term.lines[(y - HISTORY_SIZE + term.nrows - 3)])
 
 #define UPDATE_WRAP_NEXT(alt, col)                                                                  \
     do {                                                                                            \
