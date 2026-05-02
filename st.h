0968321de199ba05888673d6d4aa5fdd124b74cc
lucas-mior <lucas.mior@tutamail.com>
2026-05-02 11:03:47 -0300

    use enum types

--- st.h
+++ st.h
@@ -224,7 +224,7 @@ static struct {
     int32 top_scroll_limit;    /* top    scroll limit */
     int32 bot_scroll_limit;    /* bottom scroll limit */
     int32 mode;                /* terminal mode flags */
-    int32 esc;                 /* escape state flags */
+    enum escape_state esc;     /* escape state flags */
     char translation_table[4]; /* charset table translation */
     int32 charset;             /* current charset */
     int32 icharset;            /* selection_is_selected charset for sequence */
