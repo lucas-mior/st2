3b421c6d3704d951e585dfd48494c70ea8e594cf
lucas-mior <lucas.mior@tutamail.com>
2026-05-02 11:26:13 -0300

    wip

--- sixel.h
+++ sixel.h
@@ -54,10 +54,15 @@ typedef struct SixelState {
 struct ImageList;
 void scroll_images(int32 n);
 void delete_image(struct ImageList *im);
-int32 sixel_parser_init(SixelState *st, int32 transparent, uint32 fgcolor, uint32 bgcolor, unsigned char use_private_register, int32 cell_width, int32 cell_height);
+int32 sixel_parser_init(SixelState *st,
+		                int32 transparent,
+						uint32 fgcolor, uint32 bgcolor,
+						uchar use_private_register,
+						int32 cell_width, int32 cell_height);
 int32 sixel_parser_parse(SixelState *st, uchar *p, int32 len);
 int32 sixel_parser_set_default_color(SixelState *st);
-int32 sixel_parser_finalize(SixelState *st, struct ImageList **newimages, int32 cx, int32 cy, int32 cw, int32 ch);
+int32 sixel_parser_finalize(SixelState *st, struct ImageList **newimages,
+		                    int32 cx, int32 cy, int32 cw, int32 ch);
 void sixel_parser_deinit(SixelState *st);
 Pixmap sixel_create_clipmask(char *pixels, int32 width, int32 height);
 
