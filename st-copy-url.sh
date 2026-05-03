58c4dc4c760c33ed6a1f0e8834e8a5b3ef6ff8d0
lucas-mior <lucas.mior@tutamail.com>
2026-05-03 13:51:31 -0300

    organize script

--- st-copy-url.sh
+++ st-copy-url.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
 urlregex="(((http|https|gopher|gemini|ftp|ftps|git)://|www\.)[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./@$&%?$\#=_~-]*)|((magnet:\?xt=urn:btih:)[a-zA-Z0-9]*)"
-urls=$(sed 's/.*│//g' | tr -d '\n' | grep -aEo "$urlregex" | uniq | sed "s/\(\.\|,\|;\|\!\|\?\)$//; s/^www./http:\/\/www\./")
-[ -z "$urls" ] && exit 1
-if [ "$2" = "o" ]; then
-  chosen=$(echo "$urls" | dmenu -w "$1" -i -p 'Follow which url?' -l 10)
-  [ -z "$chosen" ] && exit 0
-  echo "$chosen" | tr -d '\n' | xclip -selection clipboard
-  setsid xdg-open "$chosen" >/dev/null 2>&1 &
-else
-  echo "$urls" | dmenu -w "$1" -p 'Copy which url?' -l 10 | tr -d '\n' | xclip -selection clipboard
+
+urls=$(sed 's/.*│//g' \
+       | tr -d '\n' \
+       | grep -aEo "$urlregex" \
+       | uniq \
+       | sed "s/\(\.\|,\|;\|\!\|\?\)$//; s/^www./http:\/\/www\./")
+
+if [ -z "$urls" ]; then
+    exit 1
 fi
+
+chosen=$(echo "$urls" | dmenu -w "$1" -i -p 'Follow which url?' -l 10)
+if [ -z "$chosen" ]; then
+    exit 0
+fi
+echo "$chosen" \
+    | tr -d '\n' \
+    | xclip -selection clipboard
+setsid xdg-open "$chosen" >/dev/null 2>&1 &
