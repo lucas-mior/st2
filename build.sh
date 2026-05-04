89a987a32bdd3d4944a5960041130c45d5f9954f
lucas-mior <lucas.mior@tutamail.com>
2026-05-04 10:24:02 -0300

    disable gcc static analyzer for now

--- build.sh
+++ build.sh
@@ -362,7 +362,7 @@ case "$target" in
     exit
     ;;
 "check")
-    CC=gcc CFLAGS="-fanalyzer" ./build.sh
+    # CC=gcc CFLAGS="-fanalyzer" ./build.sh
     scan-build --view -analyze-headers --status-bugs ./build.sh
     exit
     ;;
