eb057b89cf163e916a4988b21e57c0a3224a5942
lucas-mior <lucas.mior@tutamail.com>
2026-05-08 19:41:32 -0300

    play with plots

--- demo/plot-semi.py
+++ demo/plot-semi.py
@@ -5,17 +5,30 @@ import numpy as np
 
 plt.figure(figsize=(6,4), facecolor="none")
 
-ax = plt.axes()
-ax.set_facecolor("#111111")
+ax_left = plt.axes()
+ax_left.set_facecolor("#111111")
 
-ax.tick_params(colors="white")
-for spine in ax.spines.values():
+ax_left.tick_params(colors="white")
+for spine in ax_left.spines.values():
+    spine.set_edgecolor("white")
+
+ax_right = ax_left.twinx()
+
+ax_right.tick_params(colors="#111111")
+for spine in ax_right.spines.values():
     spine.set_edgecolor("white")
 
 x = np.linspace(0, 10, 100)
-y = np.sin(x)
+y1 = np.sin(x)
+y2 = np.exp(x)
 
-plt.plot(x, y, linewidth=2, color="cyan")
-plt.title("Sine Wave in Terminal", color="white")
+ax_left.plot(x, y1, linewidth=2, color="cyan", label="sin(x)")
+ax_right.plot(x, y2, linewidth=2, color="orange", label="exp(x)")
+
+ax_left.legend(loc="lower left", bbox_to_anchor=(0, 1), frameon=False, labelcolor="white")
+
+ax_right.legend(loc="lower right", bbox_to_anchor=(1, 1), frameon=False, labelcolor="white")
+
+plt.title("Functions", color="white", pad=25)
 
 plt.show()
