212b753ecda3f7411ff75a5d720abdadbfb26782
lucas-mior <lucas.mior@tutamail.com>
2026-05-08 20:30:43 -0300

    style (whitespace)

--- demo/plot-white.py
+++ demo/plot-white.py
@@ -25,11 +25,10 @@ y2 = np.exp(x)
 ax_left.plot(x, y1, linewidth=2, color="tab:blue", label="sin(x)")
 ax_right.plot(x, y2, linewidth=2, color="tab:orange", label="exp(x)")
 
-ax_left.legend(loc="lower left", bbox_to_anchor=(0, 1), frameon=False,
-               labelcolor="black")
-
-ax_right.legend(loc="lower right", bbox_to_anchor=(1, 1), frameon=False,
-                labelcolor="black")
+ax_left.legend(loc="lower left", bbox_to_anchor=(0, 1),
+               frameon=False, labelcolor="black")
+ax_right.legend(loc="lower right", bbox_to_anchor=(1, 1),
+                frameon=False, labelcolor="black")
 
 plt.title("Functions", color="black", pad=25)
 
