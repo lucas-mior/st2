8c6e91b2c47e0cd63aeb7f0da97915e07c080cc7
lucas-mior <lucas.mior@tutamail.com>
2026-05-10 21:28:33 -0300

    improve grid of plot

--- demo/plot-white.py
+++ demo/plot-white.py
@@ -22,13 +22,23 @@ x = np.linspace(0, 10, 100)
 y1 = np.sin(x)
 y2 = np.exp(x)
 
-ax_left.grid(color="#cccccc")
-ax_right.grid(color="#cccccc")
-
 ax_left.plot(x, y1, label="sin(x)",
              color="tab:blue", linewidth=2, linestyle="dashed")
 ax_right.plot(x, y2, label="exp(x)",
               color="tab:orange", linewidth=2)
+
+ax_left.grid(axis="x", color="#cccccc")
+
+xmin, xmax = ax_left.get_xlim()
+ax_left.set_xlim(xmin, xmax)
+ax_left.set_ylim(ax_left.get_ylim())
+ax_right.set_ylim(ax_right.get_ylim())
+
+ax_left.hlines(ax_left.get_yticks(), xmin=xmin, xmax=10, 
+               color="#cccccc", linewidth=0.8, zorder=0)
+
+ax_right.hlines(ax_right.get_yticks(), xmin=0, xmax=xmax, 
+                color="#cccccc", linewidth=0.8, zorder=0)
 
 ax_left.legend(loc="lower left", bbox_to_anchor=(0, 1),
                frameon=False, labelcolor="black")
