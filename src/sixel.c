a17a74648c590b5cc1bbd04b735f0b56606c666f
lucas-mior <lucas.mior@tutamail.com>
2026-05-22 15:46:59 -0300

    fix unrolled loop

--- src/sixel.c
+++ src/sixel.c
@@ -315,7 +315,6 @@ sixel_parser_finalize(SixelState *sixel_state, ImageList **new_images,
             uint16 *src = sixel_state->image.data + sixel_image->width*y;
             int32 x = 0;
             for (; x + 7 < w; x += 8) {
-                const uint32 zeros[8] = {0};
                 dst[0] = sixel_state->image.palette[src[0]];
                 dst[1] = sixel_state->image.palette[src[1]];
                 dst[2] = sixel_state->image.palette[src[2]];
@@ -324,10 +323,15 @@ sixel_parser_finalize(SixelState *sixel_state, ImageList **new_images,
                 dst[5] = sixel_state->image.palette[src[5]];
                 dst[6] = sixel_state->image.palette[src[6]];
                 dst[7] = sixel_state->image.palette[src[7]];
-                if (!trans
-                        && !memcmp64((void *)zeros,
-                                     &sixel_state->image.palette[src[0]],
-                                     SIZEOF(zeros))) {
+                if (!trans) {
+                    if (dst[0] == 0
+                        || dst[1] == 0
+                        || dst[2] == 0
+                        || dst[3] == 0
+                        || dst[4] == 0
+                        || dst[5] == 0
+                        || dst[6] == 0
+                        || dst[7] == 0)
                     trans = 1;
                 }
                 src += 8;
