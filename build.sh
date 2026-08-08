e4e3301bf61f973db35897477fcc5144eb8c8e19
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 20:21:35 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -86,8 +86,8 @@ CPPFLAGS="$CPPFLAGS $(pkg-config --cflags harfbuzz)"
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
 CFLAGS="$CFLAGS -Wextra -Wall"
-# CFLAGS="$CFLAGS -Werror=all -Werror=extra"
-# CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
+CFLAGS="$CFLAGS -Werror=all -Werror=extra"
+CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wno-cast-align"
 CFLAGS="$CFLAGS -Wno-cast-qual"
 CFLAGS="$CFLAGS -Wno-comma"
