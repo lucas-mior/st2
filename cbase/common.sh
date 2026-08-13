71d5cc234720de3f2d575f94f15d02f4c67bb610
lucas-mior <lucas.mior@tutamail.com>
2026-08-13 12:20:59 -0300

    update cbase

--- cbase/common.sh
+++ cbase/common.sh
@@ -24,7 +24,7 @@ error () {
 }
 
 # shellcheck source=./cbase/functions_forbidden.sh
-. ./cbase/functions_forbidden.sh
+# . ./cbase/functions_forbidden.sh
 
 common_command_exists () {
     command -v "$1" > /dev/null 2>&1
@@ -81,7 +81,7 @@ if common_command_exists zig; then
                     | grep -v -- "^m68k-" \
                     | grep -v -- "^loongarch64-linux-gnusf" \
                     | grep -v -- "^sparc-" \
-                    | grep -v -- "^sparc64-linux-gnu" \
+                    | grep -v -- "^sparc64-" \
                     | grep -v -- "^csky-")
 fi
 echo "cross_targets = $cross_targets" > /dev/null
