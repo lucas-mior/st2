9df47b46fab8c1cb3c48e149a4756c5b1d880452
lucas-mior <lucas.mior@tutamail.com>
2026-08-13 16:33:57 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -56,7 +56,7 @@ CFLAGS="$CFLAGS -Werror=all -Werror=extra"
 CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wno-type-limits"
 
-if [ "$CC" = "clang" ]; then
+if [ "$CC" = "clang" ] || [ "$CC" = "zig cc" ]; then
     CFLAGS="$CFLAGS -Weverything"
     CFLAGS="$CFLAGS -Wno-assign-enum"
     CFLAGS="$CFLAGS -Wno-bad-function-cast"
